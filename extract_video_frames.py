9cae5e412678c4b69eb11ec34d2ba15a32bac08e
Eric Jinks <ericjinks@gmail.com>
2020-02-19 05:11:52 +0000

    Add extract_video_frames script

---
+++ extract_video_frames.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+import math
+import json
+import time
+import sys
+import subprocess
+
+
+def printJson(data):
+    print(json.dumps(data))
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+
+def run(args):
+    input = args.input
+    fps = args.fps
+    output_dir = args.output_dir
+    input_filename = os.path.splitext(os.path.basename(input))[0]
+
+    if output_dir:
+        os.makedirs(output_dir, exist_ok=True)
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-hide_banner",
+            "-i",
+            input,
+            "-vf",
+            f"fps={fps}",
+            "-qscale:v",
+            "2",
+            os.path.join(output_dir, f"{input_filename}_{fps}fps_%06d.jpg"),
+        ],
+        capture_output=False,
+    )
+    print(
+        "Video frames extracted to",
+        os.path.join(output_dir, f"{input_filename}_{fps}fps_%06d.jpg"),
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create an images_list.txt from contents of image directory"
+    )
+    parser.add_argument("--input", default="./videos/video_test.mov/", required=True)
+    parser.add_argument("--output_dir", default="./output/video_frames/", required=True)
+    parser.add_argument("--fps", default=25, type=int)
+
+    args = parser.parse_args()
+
+    run(args)
+
+    # python extract_video_frames.py --input=videos/video_test.mov --output_dir=output/video_frames --fps=25
